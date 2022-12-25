a2653305733cd8f17b4f57896e1205f3d7e730bf
deformal <drake9211@gmai.com>
2022-12-26 01:57:52 +0530

    fixed-path-issue

---
+++ learning-go/hello_world/hello.go
@@ -0,0 +1,7 @@
+package manners
+
+import "fmt"
+
+func SayHello() {
+	fmt.Println("Hello world")
+}
---
+++ learning-go/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	hello "learning-go/hello_world"
+	bye "learning-go/hello_world/bye"
+)
+
+func main() {
+	hello.SayHello()
+	bye.SayBye()
+}
